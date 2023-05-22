3a8e3c6642c11a514ef51384b178cbc6ab0b6f66
edd-ie <ogendoeddie@gmail.com>
2023-05-22 18:54:12 +0300

    complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   # your code here
+  def initialize(val)
+      val.each{|key,value|
+          self.class.attr_accessor(key)
+          self.send("#{key}=", value)
+      }
+  end
 end
